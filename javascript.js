0d5a9f80bc043ef88ce8072a398240dc4b6ac8fd
Ohhgee <ztt7869@gmail.com>
2020-04-03 21:28:29 -0700

    Added VoteSites

--- javascript.js
+++ javascript.js
@@ -1,21 +1,8 @@
-var copyEmailBtn = document.querySelector('.js-emailcopybtn');  
-copyEmailBtn.addEventListener('click', function(event) {  
-  // Select the email link anchor text  
-  var emailLink = document.querySelector('.js-emaillink');  
-  var range = document.createRange();  
-  range.selectNode(emailLink);  
-  window.getSelection().addRange(range);  
-
-  try {  
-    // Now that we've selected the anchor text, execute the copy command  
-    var successful = document.execCommand('copy');  
-    var msg = successful ? 'successful' : 'unsuccessful';  
-    console.log('Copy email command was ' + msg);  
-  } catch(err) {  
-    console.log('Oops, unable to copy');  
-  }  
-
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported  
-  window.getSelection().removeAllRanges();  
+$('a.allsites').click(function(e) {
+    e.preventDefault();
+    window.open('https://minecraft-server-list.com/server/446140/vote/');
+    window.open('https://minecraft-mp.com/server/233889/vote/');
+    window.open('https://minecraft-server.net/vote/ohhgee/');
+    window.open('https://www.planetminecraft.com/server/ohhgee-1-14-4-vanilla-survival-land-claim-player-shops-community/vote/');
+    window.open('https://minecraftservers.org/vote/563817');
 });
